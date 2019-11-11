fa179377a721598fbba1f6c6f8b9153590583780
danielperr <daniel.perez079@gmail.com>
2019-11-11 20:15:43 +0200

    Reconfigured PlayingCard self drawing methods Can now present any type of card

--- taki-client-YB2020/Controls/PlayingCard.Designer.cs
+++ taki-client-YB2020/Controls/PlayingCard.Designer.cs
@@ -28,40 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayingCard));
-            this.mainLabel = new taki_client_YB2020.BorderLabel(this.components);
             this.SuspendLayout();
-            // 
-            // mainLabel
-            // 
-            this.mainLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.mainLabel.ForeColor = System.Drawing.Color.Red;
-            this.mainLabel.Location = new System.Drawing.Point(0, 0);
-            this.mainLabel.Name = "mainLabel";
-            this.mainLabel.OutlineForeColor = System.Drawing.Color.Black;
-            this.mainLabel.OutlineWidth = 4F;
-            this.mainLabel.Size = new System.Drawing.Size(124, 184);
-            this.mainLabel.TabIndex = 1;
-            this.mainLabel.Text = "TAKI";
             // 
             // PlayingCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.Controls.Add(this.mainLabel);
             this.Name = "PlayingCard";
-            this.Size = new System.Drawing.Size(124, 184);
-            this.Load += new System.EventHandler(this.PlayingCard_Load);
+            this.Size = new System.Drawing.Size(120, 180);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private BorderLabel mainLabel;
     }
 }
